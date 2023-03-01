483d4b42cb8e1fb6da806090525800e7b1966589
Pooja <poojadaf878@gmail.com>
2023-03-01 23:18:39 +0530

    calcculator app

---
+++ calculator.js/script.js
@@ -0,0 +1,34 @@
+// console.log(screen);
+
+const btn1 = document.querySelectorAll(".cal-btn");
+const arr = [...btn1];
+let value = "";
+const screen = document.querySelector('.screen');
+
+arr.forEach((btn) => {
+   
+    btn.addEventListener("click", (e) => {
+
+        let x = e.target;
+        if (x.innerText == '=') {
+            value = eval(value);
+            // console.log(value);
+            screen.innerHTML = value;
+        }
+        else if (x.innerText == 'AC') {
+            screen.innerHTML = " ";
+            value = " ";
+        }
+        else if (x.innerText == 'clear') {
+            value = [...value].slice(0,-1).join('');
+            screen.innerText = value;
+            // console.log(value);
+        }
+        else{
+            value = value + x.innerText;
+            // console.log(value);
+            screen.innerText = value;
+
+        }
+    });
+})
