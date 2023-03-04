e1c50a990145889f39feba8df2855c912d41de45
Pooja <poojadaf878@gmail.com>
2023-03-04 22:26:02 +0530

    variables01

--- variables.js/script.js
+++ variables.js/script.js
@@ -1,27 +1,42 @@
-const uid = "abc123";
+// const uid = "abc123";
 
-var firstName = "Pooja";
-var lastname = "Daf";
-var email =  "xxxxxxxx123";
-var password = "xxxxxxxx";
-var confirmPassword = "xxxxxxxx";
-var Country = "India";
-var State = "Maharashtra";
-var courseCount = 0;
-var isLoggedInFromGOOGLE = false;
-var isLoggedInFromFaceBook = false;
+// var firstName = "Pooja";
+// var lastname = "Daf";
+// var email =  "xxxxxxxx123";
+// var password = "xxxxxxxx";
+// var confirmPassword = "xxxxxxxx";
+// var Country = "India";
+// var State = "Maharashtra";
+// var courseCount = 0;
+// var isLoggedInFromGOOGLE = false;
+// var isLoggedInFromFaceBook = false;
 
 
-console.log(`
- With Unique ID: ${uid}
- my name is: ${firstName}
- my lastname is: ${lastname}
-and my email is: ${email}
-my password is: ${password}
-i live in: ${Country}
-in the state: ${State}
-courseCount is: ${courseCount}
-isLoggedInFromGOOGLE: ${isLoggedInFromGOOGLE}
-isLoggedInFromFaceBook: ${isLoggedInFromFaceBook}
-`)
+// console.log(`
+//  With Unique ID: ${uid}
+//  my name is: ${firstName}
+//  my lastname is: ${lastname}
+// and my email is: ${email}
+// my password is: ${password}
+// i live in: ${Country}
+// in the state: ${State}
+// courseCount is: ${courseCount}
+// isLoggedInFromGOOGLE: ${isLoggedInFromGOOGLE}
+// isLoggedInFromFaceBook: ${isLoggedInFromFaceBook}
+// `)
+
+var num1 = 220;
+var num2 = 399;
+console.log(num1+num2);
+
+var answer = num1>num2;
+console.log(answer);
+
+var lp = 799;
+var sp = 199;
+var dicountPercent =(799-199)/799*100;
+console.log("discountpercantis",dicountPercent);
+
+ var DisplaydicountPercent = Math.round(dicountPercent);
+console.log(DisplaydicountPercent+"% OFF");
 
