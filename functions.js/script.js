e432508af8df75fe12cdb1be9b0606d216599277
Pooja <poojadaf878@gmail.com>
2023-04-06 21:27:33 +0530

    fun & object in js

--- functions.js/script.js
+++ functions.js/script.js
@@ -1,64 +1,80 @@
 
-function URL(url, domain){
-    let con = 'https://';
-    let result = con + url + domain;
-    return result;
-}
+// function URL(url, domain){
+//     let con = 'https://';
+//     let result = con + url + domain;
+//     return result;
+// }
 
-let site = URL('ineuron', '.ai');
-console.log(site);
-
-
-
-function sum(arr){
- let sum = 0;
-  for(let i = 0; i < arr.length; i++){
-   sum = sum + arr[i];
-  }
-  return sum;
-}
-
-let sumarray = [1,2,3,4,5,6,7];
-let arrayresult = sum(sumarray);
-console.log(arrayresult);
+// let site = URL('ineuron', '.ai');
+// console.log(site);
 
 
-function pooja(){
-    let sum = 0;
-    for(let i = 0; i< arguments.length; i++){
-sum = sum + arguments[i];
-    }
-    return sum;
-}
 
-console.log(pooja(23,33,2,4,6,7,8,9));
+// function sum(arr){
+//  let sum = 0;
+//   for(let i = 0; i < arr.length; i++){
+//    sum = sum + arr[i];
+//   }
+//   return sum;
+// }
+
+// let sumarray = [1,2,3,4,5,6,7];
+// let arrayresult = sum(sumarray);
+// console.log(arrayresult);
+
+
+// function pooja(){
+//     let sum = 0;
+//     for(let i = 0; i< arguments.length; i++){
+// sum = sum + arguments[i];
+//     }
+//     return sum;
+// }
+
+// console.log(pooja(23,33,2,4,6,7,8,9));
 
 
 
 // arrow function
 
- let me = (var1, var2) => {
-    console.log('I am a student', var1, var2);
- };
+//  let me = (var1, var2) => {
+//     console.log('I am a student', var1, var2);
+//  };
 
- me(1,2);
+//  me(1,2);
 
 
 
 //  Object
 
-let username = {
-    firstname:'pooja',
-    lastname: 'daf',
-    role: 'student',
-    logincount: 25,
+// let username = {
+//     firstname:'pooja',
+//     lastname: 'daf',
+//     role: 'student',
+//     logincount: 25,
+// }
+// console.log(username);
+
+// username.logincount = 45;
+// console.log(username);
+
+
+// for(let x in username){
+//     console.log(username[x]);
+// }
+
+
+// console.log(this);
+
+
+// objects
+
+const powers = {
+    fly: true,
+    coardinate: Math.random()+2
 }
-console.log(username);
 
-username.logincount = 45;
-console.log(username);
-
-
-for(let x in username){
-    console.log(username[x]);
-}
+let obj3 = Object.create(powers);
+console.log(obj3);
+console.log(obj3.coardinate);
+console.log(Object.getPrototypeOf(obj3));
