0ab66d99718e51cb976fc2e9810d21bd718378d0
Pooja <poojadaf878@gmail.com>
2023-04-14 17:22:44 +0530

    loops in js

---
+++ functions.js/loops.js
@@ -0,0 +1,23 @@
+
+// for
+const  mystates = ["maharashtra", "Delhi", "Assam", 1456, "Tamil Nadu"];
+
+for(let i=0; i<mystates.length; i++){
+    console.log(mystates[i]);
+
+}
+
+
+
+    
+// for of
+for (const x of mystates) {
+    return x;
+    
+}
+console.log(x);
+
+
+// // for each
+// const  mynewstates = ["maharashtra", "Delhi", "Assam", "Tamil Nadu"];
+//   mynewstates.forEach((x) => console.log(x));
